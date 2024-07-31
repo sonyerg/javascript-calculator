3383dc458980225164c293c7329e557975d4003c
sonyerg <ericksonrguinto@gmail.com>
2024-08-01 10:50:29 +1200

    passed test

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,87 @@ function App() {
   const [answer, setAnswer] = useState("");
   const [expression, setExpression] = useState("0");
 
-  //TODO: buttonpress;
-  function buttonPress(symbol: string) {}
+  const et = expression.trim();
 
-  //TODO: calculate
-  function calculate() {}
+  const isOperator = (symbol: string) => {
+    return /[*/+-]/.test(symbol);
+  };
+
+  function buttonPress(symbol: string) {
+    if (symbol === "clear") {
+      setAnswer("");
+      setExpression("0");
+    } else if (symbol === "negative") {
+      if (answer === "") return;
+      setAnswer(
+        answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+      );
+    } else if (symbol === "percent") {
+      if (answer === "") return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)) {
+      // if (isOperator(et.charAt(et.length - 1))) return;
+      setExpression(et + " " + symbol + " ");
+    } else if (symbol === "=") {
+      calculate();
+    } else if (symbol === "0") {
+      if (expression.charAt(0) !== "0") {
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === ".") {
+      // split by operators and get last number
+      const lastNumber = expression.split(/[-+/*]/g).pop();
+      if (!lastNumber) return;
+      console.log("lastNumber :>> ", lastNumber);
+      // if last number already has a decimal, don't add another
+      if (lastNumber?.includes(".")) return;
+      setExpression(expression + symbol);
+    } else {
+      if (expression.charAt(0) === "0") {
+        setExpression(expression.slice(1) + symbol);
+      } else {
+        setExpression(expression + symbol);
+      }
+    }
+  }
+
+  function calculate() {
+    // check if last char is an operator. If true, return.
+    // length - 1 means the last character
+    if (isOperator(et.charAt(et.length - 1))) return;
+
+    const parts = et.split(" ");
+    const newParts: string[] = [];
+
+    // go through parts backwards
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
+        newParts.unshift(parts[i]);
+        let j = 0;
+        let k = i - 1;
+        while (isOperator(parts[k])) {
+          k--;
+          j++;
+        }
+        i -= j;
+      } else {
+        newParts.unshift(parts[i]);
+      }
+    }
+    const newExpression = newParts.join(" ");
+    if (isOperator(newExpression.charAt(0))) {
+      setAnswer(eval(answer + newExpression) as string);
+    } else {
+      setAnswer(eval(newExpression) as string);
+    }
+    setExpression("");
+  }
 
   return (
     <div className="container">
       <h1>JS Calculator</h1>
       <div id="calculator">
-        <div id="display">
+        <div id="display" style={{ textAlign: "right" }}>
           <div id="answer">{answer}</div>
           <div id="expression">{expression}</div>
         </div>
@@ -34,8 +104,8 @@ function App() {
           +/-
         </button>
         <button
-          id="percentage"
-          onClick={() => buttonPress("percentage")}
+          id="percent"
+          onClick={() => buttonPress("percent")}
           className="light-gray"
         >
           %
